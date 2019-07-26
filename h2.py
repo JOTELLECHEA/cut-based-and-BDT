a43b5cf3908f7b468fea369a5d964c9a77f59b00
JonathanTellechea <jtellece@ucsc.edu>
2019-07-25 22:33:22 -0700

    fixed h2.py

--- h2.py
+++ h2.py
@@ -6,7 +6,11 @@ g = ROOT.TFile('all_hist.root')
 #---------------------------- load Histogram ----------------------------------#
 Dict0 = {1:'ttHH' ,2:'ttH',3:'ttZ',4:'ttbb'}
 Dict1 = {}
-for i in xrange(7):
-    for j in xrange(1,4):
-        Dict1[i] = Dict0[j] + str(i) + ' = g.Get(' + Dict0[j] + str(i) + ')'
-        print Dict1[i]
+count = 1
+for i in xrange(8):
+     for j in xrange(1,5):
+         # Dict1[i]={}
+         histtoget=Dict0[j]+str(i)
+         print histtoget
+         Dict1[count]=g.Get(histtoget)
+         count += 1
