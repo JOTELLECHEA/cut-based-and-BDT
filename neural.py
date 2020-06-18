18afb84da9ea24e455a9a49de45a5ed9b351472e
jonathanTellechea <jtellece@ucsc.edu>
2020-06-17 17:36:10 -0700

    converted test neural network to tthh

--- neural.py
+++ neural.py
@@ -2,7 +2,10 @@
 # Adviser    : Mike Hance, Phd
 # Research   : Using a neural network to maximize the significance of tttHH production.
 # Reference  :http://cdsweb.cern.ch/record/2220969/files/ATL-PHYS-PUB-2016-023.pdf
-
+from ROOT import*
+from root_numpy import array2root,root2array,rec2array
+import csv,sys
+import argparse
 import pandas as pd
 import numpy as np
 from sklearn import preprocessing
@@ -10,45 +13,81 @@ le = preprocessing.LabelEncoder()
 from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import classification_report, confusion_matrix
-
-
-
-# Location of dataset
-url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
-
-# Assign colum names to the dataset
-names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'Class']
-
-# Read dataset to pandas dataframe
-irisdata = pd.read_csv(url, names=names)
-
-
-# Assign data from first four columns to X variable
-X = irisdata.iloc[:, 0:4]
-
-# Assign data from first fifth columns to y variable
-y = irisdata.select_dtypes(include=[object])
-
-np.array(['Iris-setosa', 'Iris-versicolor', 'Iris-virginica'], dtype=object)
-
-
-
-y = y.apply(le.fit_transform)
-
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)
+import warnings
+warnings.filterwarnings('ignore')
+
+# file= 'ROC_data'
+tree = 'OutputTree'
+
+parser = argparse.ArgumentParser(description= 'BDT for ttHH and background')
+parser.add_argument("--branch", default='wrong', type=str, help= "Use '--branch=' followed by a branch_name")
+
+args = parser.parse_args()
+branch = str(args.branch)
+
+bn_phase1   = """njet""".split(",")
+bn_phase2   = """njet,btag,srap""".split(",")
+bn_phase3   = """njet,btag,srap,cent,m_bb,h_b""".split(",")
+bn_phase4   = """njet,btag,srap,cent,m_bb,h_b,mt1,mt2,mt3,dr1,dr2,dr3""".split(",")
+bn_phase5   = """njet,btag,srap,cent,m_bb,h_b,mt1,mt2,mt3,dr1,dr2,dr3,chi""".split(",")
+
+while True:
+    try:
+        if branch == 'phase1':
+            print 'You selected option:', branch
+            branch_names = bn_phase1
+            # name = file + '_' + branch + '.csv'
+            break
+        elif branch == 'phase2':
+            print 'You selected option:', branch
+            branch_names = bn_phase2
+            # name = file + '_' + branch + '.csv'
+            break
+        elif branch == 'phase3':
+            print 'You selected option:', branch
+            branch_names = bn_phase3
+            # name = file + '_' + branch + '.csv'
+            break
+        elif branch == 'phase4':
+            print 'You selected option:', branch
+            branch_names = bn_phase4
+            # name = file + '_' + branch  + '.csv'
+            break
+        elif branch == 'phase5':
+            print 'You selected option:', branch
+            branch_names = bn_phase5
+            # name = file + '_' + branch + '.csv'
+            break
+        elif branch == 'wrong':
+            sys.exit('Need to pass a variable, use --h for options')
+    except NameError:
+        system('clear')
+        prRed('*******************Invalid option **************\n')
+        break
 
 
+branch_names = [c.strip() for c in branch_names]
+branch_names = (b.replace(" ", "_") for b in branch_names)
+branch_names = list(b.replace("-", "_") for b in branch_names)
 
-scaler = StandardScaler()
-scaler.fit(X_train)
+signal = root2array('new_signal_tthh.root',tree, branch_names, include_weight=True)
+signal = rec2array(signal)
 
-X_train = scaler.transform(X_train)
-X_test = scaler.transform(X_test)
+bg_ttbb   = root2array('new_background_ttbb.root', tree, branch_names, include_weight=True)
+bg_ttZ    = root2array('new_background_ttZ.root' , tree, branch_names, include_weight=True)
+bg_ttH    = root2array('new_background_ttH.root' , tree, branch_names, include_weight=True)
+background    = np.concatenate((bg_ttbb,bg_ttH,bg_ttZ))
+background    = rec2array(background)
 
-
+X = np.concatenate((signal, background)) 
+y = np.concatenate((np.ones(signal.shape[0]), np.zeros(background.shape[0])))
+X_dev,X_eval, y_dev,y_eval = train_test_split(X, y, test_size = 0.10, random_state=42)
+X_train,X_test, y_train,y_test, = train_test_split(X_dev, y_dev, test_size = 0.5,random_state=42)  
+# mlp = MLPClassifier(hidden_layer_sizes=(10, 10, 10), max_iter=1000)
+# mlp.fit(X_train, y_train.values.ravel())
 mlp = MLPClassifier(hidden_layer_sizes=(10, 10, 10), max_iter=1000)
-mlp.fit(X_train, y_train.values.ravel())
+mlp.fit(X_train, y_train)
 
 predictions = mlp.predict(X_test)
 
