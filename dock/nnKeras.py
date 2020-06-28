d16a6ea8ec65c22238263ff0590fc16f156dfb1c
jonathanTellechea <jtellece@ucsc.edu>
2020-06-27 23:41:37 -0700

    moved to dock

---
+++ dock/nnKeras.py
@@ -0,0 +1,6 @@
+import tensorflow.contrib.keras as keras
+from keras.layers import Dense, Dropout, Activation, Flatten, Convolution2D, MaxPooling2D	
+# from python.keras.models import Sequential
+# model = Sequential()
+# from tensorflow.python.keras.utils import np_utils
+print('Done')
